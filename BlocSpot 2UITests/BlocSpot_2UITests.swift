975558be3a14ac108d868feab6f2874d144cf457
Paul Lozada <paul_lozada@icloud.com>
2015-06-11 08:40:23 -0600

    First commit

---
+++ BlocSpot 2UITests/BlocSpot_2UITests.swift
@@ -0,0 +1,35 @@
+//
+//  BlocSpot_2UITests.swift
+//  BlocSpot 2UITests
+//
+//  Created by Paul Lozada on 2015-06-11.
+//  Copyright © 2015 Paul Lozada. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class BlocSpot_2UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
